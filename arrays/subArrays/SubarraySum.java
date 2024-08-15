cea70b60e37c2a0522b4ce362d56f7e1503e8cd3
Kalpana R Kale <kalekalpanar@gmail.com>
2024-08-15 21:11:03 +0530

    Added SubarraySum.java
    The function  prefixSumArray computes and prints the maximum and minimum sum of all subarrays using a prefix sum array.

---
+++ arrays/subArrays/SubarraySum.java
@@ -0,0 +1,43 @@
+public class SubarraySum {
+
+    // Method to calculate the maximum and minimum sum of subarrays using a prefix sum array
+    public static void prefixSumArray(int numbers[]) {
+        int maxSumArray = Integer.MIN_VALUE;
+        int minSumArray = Integer.MAX_VALUE;
+        int prefixArr[] = new int[numbers.length];
+
+        // Create prefix sum array
+        prefixArr[0] = numbers[0];
+        for (int i = 1; i < prefixArr.length; i++) {
+            prefixArr[i] = prefixArr[i - 1] + numbers[i];
+        }
+
+        // Calculate max and min sum of subarrays
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                int sumArray = (i == 0) ? prefixArr[j] : prefixArr[j] - prefixArr[i - 1];
+
+                if (maxSumArray < sumArray) {
+                    maxSumArray = sumArray;
+                }
+                if (minSumArray > sumArray) {
+                    minSumArray = sumArray;
+                }
+            }
+        }
+
+        System.out.println("\nSum of Sub-Arrays using Prefix Sum Array");
+        System.out.println("Max sum of Subarray = " + maxSumArray);
+        System.out.println("Min sum of Subarray = " + minSumArray);
+    }
+
+    public static void main(String[] args) {
+        int arr1[] = {3, -4, 8, 10, 16};
+        int arr2[] = {4, -2, -7, 8, 10, 1, -4};
+
+        prefixSumArray(arr1);
+        prefixSumArray(arr2);
+    }
+}
+//Time Complexity: O(n<sup>2</sup>)
+//Space Complexity: O(n)
